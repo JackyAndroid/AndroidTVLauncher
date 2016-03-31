11cba84be80e5bf02367bf20ee5f453f6c7edab4
jacky <jacky.android@foxmail.com>
2016-03-31 11:12:48 +0800

    remove unused lib

--- app/src/main/java/com/jacky/launcher/application/ClientApplication.java
+++ app/src/main/java/com/jacky/launcher/application/ClientApplication.java
@@ -3,11 +3,6 @@ package com.jacky.launcher.application;
 import android.app.Application;
 import android.content.Context;
 
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-
 public class ClientApplication extends Application {
 
     /**
@@ -27,30 +22,9 @@ public class ClientApplication extends Application {
     public void onCreate() {
         super.onCreate();
         this.context = getApplicationContext();
-        initImageLoader(getApplicationContext());
     }
 
    public static Context getContext() {
         return context;
-    }
-
-    /**
-     * init UIL ImageLoader
-     */
-    public static void initImageLoader(Context context) {
-        // This configuration tuning is custom. You can tune every option, you
-        // may tune some of them,
-        // or you can create default configuration by
-        // ImageLoaderConfiguration.createDefault(this);
-        // method.
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-                context).threadPriority(Thread.NORM_PRIORITY - 2)
-                .denyCacheImageMultipleSizesInMemory()
-                .discCacheFileNameGenerator(new Md5FileNameGenerator())
-                .tasksProcessingOrder(QueueProcessingType.LIFO)
-                .writeDebugLogs() // Remove for release app
-                .build();
-        // Initialize ImageLoader with configuration.
-        ImageLoader.getInstance().init(config);
     }
 }
