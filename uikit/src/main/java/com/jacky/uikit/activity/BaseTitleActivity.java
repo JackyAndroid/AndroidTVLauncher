954d9e5dc9c4b23ad7858feac8c715f3c87df382
jacky <jacky.android@foxmail.com>
2016-07-01 17:26:51 +0800

    fix bug

--- uikit/src/main/java/com/jacky/uikit/activity/BaseTitleActivity.java
+++ uikit/src/main/java/com/jacky/uikit/activity/BaseTitleActivity.java
@@ -3,7 +3,11 @@ package com.jacky.uikit.activity;
 import android.support.v4.app.FragmentActivity;
 
 /**
+ * Title Activity
+ *
  * @author jacky
+ * @since 2016.7.1
+ * @version 1.0
  */
 public abstract class BaseTitleActivity extends FragmentActivity {
 
