e275968fcead683441b076f6c3236ddbb7abc5c3
jacky <jacky.android@foxmail.com>
2016-07-19 20:00:41 +0800

    add video row pic

--- CatLauncher/src/main/java/com/jacky/catlauncher/ui/MainActivity.java
+++ CatLauncher/src/main/java/com/jacky/catlauncher/ui/MainActivity.java
@@ -112,11 +112,22 @@ public class MainActivity extends Activity {
     }
 
     private void addVideoRow() {
+        String urls[] = {
+                "http://pic1.win4000.com/wallpaper/4/50a4a3c70e0b6.jpg",
+                "http://pic.nipic.com/2008-07-01/20087111641251_2.jpg",
+                "http://pic6.nipic.com/20100402/4452376_013049940716_2.jpg",
+                "http://pic.nipic.com/2007-12-15/2007121514361888_2.jpg",
+                "http://img14.poco.cn/mypoco/myphoto/20130219/19/66523397201302191911218443817764509_001.jpg",
+                "http://pic31.nipic.com/20130710/3606040_162007539156_2.jpg",
+                "http://pic22.nipic.com/20120620/1931052_155503912000_2.jpg",
+                "http://img.zcool.cn/community/0335a8c554c70c700000158fcecac3c.jpg",
+                "http://image5.tuku.cn/wallpaper/Movie%20Wallpapers/4016_2560x1600.jpg",
+        };
         int cardCount = 10;
         String headerName = getResources().getString(R.string.app_header_video_name);
         ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(new CardPresenter());
         for (int i = 0; i < cardCount; i++) {
-            listRowAdapter.add("");
+            listRowAdapter.add(urls[i]);
         }
         HeaderItem header = new HeaderItem(0, headerName);
         rowsAdapter.add(new ListRow(header, listRowAdapter));
